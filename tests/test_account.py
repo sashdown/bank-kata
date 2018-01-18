0c85c70f7f739096bcfa2fc0af654fab416894be
Simon Ashdown <swashdown@gmail.com>
2018-01-18 17:02:12 +0000

    Add test for account transfer

--- tests/test_account.py
+++ tests/test_account.py
@@ -17,3 +17,18 @@ def test_create_account():
     assert account.balance == BALANCE
 
 
+def test_transfer_from_account():
+    # Given
+    AMOUNT = 100
+
+    sending_account =  Account(0, AMOUNT)
+    receiving_account = Account(1, 0)
+
+    # When
+
+    sending_account.transfer(to=receiving_account, amount = AMOUNT)
+
+    # Then
+
+    assert  sending_account.balance == 0
+    assert receiving_account == AMOUNT
