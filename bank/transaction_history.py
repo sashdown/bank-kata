22a487f7b4a3b1e94b2350a0a2009e72299de18a
Simon Ashdown <swashdown@gmail.com>
2018-01-19 11:27:52 +0000

    add implementation for searching by payee

--- bank/transaction_history.py
+++ bank/transaction_history.py
@@ -1,3 +1,10 @@
 class TransactionHistory(list):
-    def find_transactions(self, payer):
-        return [transaction for transaction in self if transaction.payer == payer ]
+    def find_transactions(self, **kwargs):
+        payer = kwargs.get("payer", None)
+        payee = kwargs.get("payee", None)
+
+        if payer:
+            return [transaction for transaction in self if transaction.payer == payer]
+
+        if payee:
+            return [transaction for transaction in self if transaction.payee == payee]
