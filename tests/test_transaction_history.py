beea547078ba7b3ea88713a15bca5aa7da8a4683
Simon Ashdown <swashdown@gmail.com>
2018-01-19 11:20:49 +0000

    improve test readability

--- tests/test_transaction_history.py
+++ tests/test_transaction_history.py
@@ -4,16 +4,17 @@ from bank.account import Transaction
 def test_find_single_transaction_in_history(transaction_history, payee_account, payer_account):
 
     # Given
-    AMOUNT = 100
-    transaction = Transaction(payee=payee_account, payer=payer_account, amount = AMOUNT)
+
+    transaction = Transaction(payee=payee_account, payer=payer_account, amount=100)
     transaction_history.append(transaction)
+
     # When
     matching_transactions = transaction_history.find_transactions(payer=payer_account)
 
     # Then
 
     assert len (matching_transactions) == 1
-    assert matching_transactions[0] == transaction
+    assert transaction in matching_transactions
 
 
 def test_find_multiple_transactions_in_history(transaction_history, payee_account, payer_account):
@@ -44,7 +45,7 @@ def test_find_single_transaction_in_history_containing_other_account_transaction
     # Given
 
     target_transaction = Transaction(payee=payee_account, payer=payer_account, amount = 100 )
-    other_transaction = Transaction(payee=payee_account, payer=alternative_payer_account, amount= 55)
+    other_transaction  = Transaction(payee=payee_account, payer=alternative_payer_account, amount= 55)
 
     transaction_history.append(other_transaction)
     transaction_history.append(target_transaction)
@@ -54,5 +55,4 @@ def test_find_single_transaction_in_history_containing_other_account_transaction
 
     # Then
 
-    assert len (matching_transactions) == 1
-    assert matching_transactions[0] == target_transaction
+    assert [ target_transaction ] == matching_transactions
