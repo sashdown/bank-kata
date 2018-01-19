1965f596fc3e7084b60b49afe24de3e79c640d43
Simon Ashdown <swashdown@gmail.com>
2018-01-19 11:16:17 +0000

    test multiple matching transactions returned from transaction_history

--- tests/test_transaction_history.py
+++ tests/test_transaction_history.py
@@ -15,6 +15,27 @@ def test_find_single_transaction_in_history(transaction_history, payee_account, 
     assert len (matching_transactions) == 1
     assert matching_transactions[0] == transaction
 
+
+def test_find_multiple_transactions_in_history(transaction_history, payee_account, payer_account):
+
+    # Given
+
+    first_transaction = Transaction(payee=payee_account, payer=payer_account, amount = 1)
+    transaction_history.append(first_transaction)
+
+    second_transaction = Transaction(payee=payee_account, payer=payer_account, amount = 2)
+    transaction_history.append(second_transaction)
+
+    # When
+    matching_transactions = transaction_history.find_transactions(payer=payer_account)
+
+    # Then
+
+    assert 2 == len (matching_transactions)
+    assert first_transaction in matching_transactions
+    assert second_transaction in matching_transactions
+
+
 def test_find_single_transaction_in_history_containing_other_account_transactions(transaction_history,
                                                                       payee_account,
                                                                       payer_account,
